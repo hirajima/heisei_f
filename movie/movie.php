d1ddc7871e7b52e977889486d4ebe07172b7bca8
hirajima <mihasa7373@gmail.com>
2020-09-08 05:00:26 +0900

    rr

--- movie/movie.php
+++ movie/movie.php
@@ -1,3 +1,14 @@
 <?php
 echo "0907";
+
  ?>
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <video src="/../runa/MOVI000.AVI"></video>
+  </body>
+</html>
